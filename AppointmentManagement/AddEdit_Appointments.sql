2d9900748b6f36c16c0c1cf55b5da6b33a1f9890
Kriz-21 <krishnakanth.m21@gmail.com>
2024-11-19 11:19:34 +0530

    UpdateAddEdit_Appointments.sql

--- AppointmentManagement/AddEdit_Appointments.sql
+++ AppointmentManagement/AddEdit_Appointments.sql
@@ -11,7 +11,6 @@ VALUES (3, 2, '2024-11-25 15:00:00', 'Session with client feedback');
  -- ///Edit Existing Appointment
 
 sql
-Copy code
 UPDATE appointments 
 SET scheduled_time = '2024-11-26 10:00:00', status = 'Rescheduled'
 WHERE id = 1;
